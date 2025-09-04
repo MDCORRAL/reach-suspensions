0f852c7ede53ec08b606a3dfb0fdf8ea160dce32
Michaeldcorral <michaeldcorral@gmail.com>
2025-09-04 12:32:33 -0700

    update

--- R/01b_ingest_demographics.R
+++ R/01b_ingest_demographics.R
@@ -13,7 +13,7 @@ suppressPackageStartupMessages({
 
 # -------- Config -------------------------------------------------------------
 # Option A: absolute path (works today)
-DEMO_DATA_PATH <- "/Users/michaelcorral/Library/CloudStorage/GoogleDrive-mdcorral@g.ucla.edu/.shortcut-targets-by-id/1qNAOKIg0UjuT3XWFlk4dkDLN6UPWJVGx/Center for the Transformation of Schools/Research/CA Race Education And Community Healing (REACH)/2. REACH Network (INTERNAL)/15. REACH Baseline Report_Summer 2025/6. R Data Analysis Project Folders/1. 250828_R Folder/copy_CDE_suspensions_1718-2324_sc_oth.xlsx"
+DEMO_DATA_PATH <- "/Users/michaelcorral/Library/CloudStorage/GoogleDrive-mdcorral@g.ucla.edu/.shortcut-targets-by-id/1qNAOKIg0UjuT3XWFlk4dkDLN6UPWJVGx/Center for the Transformation of Schools/Research/CA Race Education And Community Healing (REACH)/2. REACH Network (INTERNAL)/15. REACH Baseline Report_Summer 2025/6. R Data Analysis Project Folders/reach-suspensions/data-raw/copy_CDE_suspensions_1718-2324_sc_oth.xlsx"
 
 # Option B: if you ever move the XLSX into your repo:
 # DEMO_DATA_PATH <- here("data-stage", "copy_CDE_suspensions_1718-2324_sc_oth.xlsx")
